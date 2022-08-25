b80cb6e5f521771ffb55b5de71cb2f807d69eab3
gustavoserres <gusserres@gmail.com>
2022-08-25 19:34:49 +0100

    Ajustes de variáveis

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,6 +1,7 @@
+
 resource "aws_instance" "dev-001" {
   provider = aws.eu-west-2
-  ami = "ami-00785f4835c6acf64"
+  ami = var.aws_amis.amazon-linux-eu-west-3
   instance_type = "t2.micro"
   key_name = "terraform-aws"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh-2.id}"]
@@ -11,10 +12,10 @@ resource "aws_instance" "dev-001" {
 
 resource "aws_instance" "dev-002" {
   provider = aws.eu-west-3
-  ami = "ami-0eb375f24fdf647b8"
+  ami = var.aws_amis.amazon-linux-eu-west-2
   instance_type = "t2.micro"
   key_name = "terraform-aws"
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh-3.id}"]
+  vpc_security_group_ids = [ "${aws_security_group.allow_ssh-3.id}" ]
   tags = {
       "Name" = "dev-002"
   }
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,20 +6,19 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-  access_key = aws_access_key
-  secret_key = aws_secret_key
-}
-
 provider "aws" {
   alias = "eu-west-2"
   region = "eu-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 provider "aws" {
   alias = "eu-west-3"
   region = "eu-west-3"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 variable aws_access_key {}
 variable aws_secret_key {}
+variable aws_amis {}
